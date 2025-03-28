4aeaf839d9e397fb2d765597be2f0142588b8f94
Juan Gonzalez <jgfm@novonordisk.com>
2025-03-28 22:56:08 +0100

    fix local registry setup
    Signed-off-by: Casper Guldbech Nielsen <scni@novonordisk.com>

--- quickstarts/06-k8s-multi-agent-workflow/install.sh
+++ quickstarts/06-k8s-multi-agent-workflow/install.sh
@@ -1,67 +1,75 @@
 #!/bin/bash
 
-export DOCKER_DEFAULT_PLATFORM=linux/arm64/v8
+unset DOCKER_DEFAULT_PLATFORM
 
 BASE_DIR=$(dirname "$0")
 
-OS=$(uname -s)
-
-if [ "$OS" = "Darwin" ]; then
-    echo "Detected macOS"
-    export DOCKER_DEFAULT_PLATFORM="linux/arm64/v8"
-elif [ "$OS" = "Linux" ]; then
-    echo "Detected Linux"
-    export DOCKER_DEFAULT_PLATFORM="linux/amd64"
-    # Prompt for sudo access upfront, so we can add the certificate to the system trust store
-    sudo ls / &> /dev/null
-else
-    echo "Unknown OS: $OS"
+# Create Registry
+REG_NAME='dapr-registry'
+REG_PORT='5001'
+if [ "$(docker inspect -f '{{.State.Running}}' "${REG_NAME}" 2>/dev/null || true)" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${REG_PORT}:5000" --network bridge --name "${REG_NAME}" \
+    registry:2
 fi
+
+# Create kind cluster with registry config
+CLUSTER_NAME='dapr-agents'
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+name: ${CLUSTER_NAME}
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  image: kindest/node:v1.32.3@sha256:b36e76b4ad37b88539ce5e07425f77b29f73a8eaaebf3f1a8bc9c764401d118c
+- role: worker
+  image: kindest/node:1.32.3@sha256:b36e76b4ad37b88539ce5e07425f77b29f73a8eaaebf3f1a8bc9c764401d118c
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry]
+    config_path = "/etc/containerd/certs.d"
+EOF
+
+# Add the registry to the nodes
+REGISTRY_DIR="/etc/containerd/certs.d/localhost:${REG_PORT}"
+for node in $(kind get nodes -n ${CLUSTER_NAME}); do
+  docker exec "${node}" mkdir -p "${REGISTRY_DIR}"
+  cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${REGISTRY_DIR}/hosts.toml"
+[host."http://${REG_NAME}:5000"]
+EOF
+done
+
+# Connect the registry to the cluster network
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${REG_NAME}")" = 'null' ]; then
+  docker network connect "kind" "${REG_NAME}"
+fi
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${REG_PORT}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
 
 build_images () {
   echo "Building images... This takes a while..."
-  docker-compose -f docker-compose.yaml build --no-cache &> /dev/null
+  docker-compose -f docker-compose.yaml build --no-cache
   echo "Images built!"
   echo "Pushing images to local registry... This takes a while..."
-  docker push registry.registry:5500/dapr-client:latest 
-  docker push registry.registry:5500/workflow-llm:latest
-  docker push registry.registry:5500/elf:latest 
-  docker push registry.registry:5500/hobbit:latest
-  docker push registry.registry:5500/wizard:latest
+  docker push localhost:5001/dapr-client:latest 
+  docker push localhost:5001/workflow-llm:latest
+  docker push localhost:5001/elf:latest 
+  docker push localhost:5001/hobbit:latest
+  docker push localhost:5001/wizard:latest
   echo "Images pushed!"
 }
-
-echo "Creating kind cluster..."
-kind create cluster --config ${BASE_DIR}/kind-cluster.yaml &>/dev/null
-echo "Cluster is ready!"
-
-# install network
-echo "installing Calico..."
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml &> /dev/null
-kubectl  create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/custom-resources.yaml &> /dev/null
-echo "Calico installed!"
-
-### Create local kind registry ####
-mkdir -p ${BASE_DIR}/.tmp &> /dev/null
-openssl genpkey -algorithm RSA -out ${BASE_DIR}/.tmp/registry.key &> /dev/null
-openssl req -new -x509 -days 365 -keyout ${BASE_DIR}/.tmp/registry.key -out ${BASE_DIR}/.tmp/registry.crt -config ${BASE_DIR}/openssl.cnf -nodes &> /dev/null
-
-
-kubectl create namespace registry &> /dev/null
-kubectl create secret tls registry-tls \
-  --cert=${BASE_DIR}/.tmp/registry.crt \
-  --key=${BASE_DIR}/.tmp/registry.key -n registry -o yaml --dry-run=client | kubectl apply -f - &> /dev/null
-kubectl apply -f ${BASE_DIR}/registry.yaml &> /dev/null
-
-echo "Installing registry certificate to local system trust store"
-if [ "$OS" = "Darwin" ]; then
-    sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ${BASE_DIR}/.tmp/registry.crt
-elif [ "$OS" = "Linux" ]; then
-    sudo cp ${BASE_DIR}/.tmp/registry.crt /usr/local/share/ca-certificates/registry-ca.crt &> /dev/null
-    sudo update-ca-certificates &> /dev/null
-else
-    echo "Registry certificate is not installed due to unsupported OS: $OS"
-fi
 
 echo "Installing Dapr..."
 helm repo add dapr https://dapr.github.io/helm-charts/ &>/dev/null && \
