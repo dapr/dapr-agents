b6089f551b57fd42a2caf0b30c2a29b40582b1d1
Casper Guldbech Nielsen <scni@novonordisk.com>
2025-04-13 16:25:25 +0200

    Fix: W503
    Signed-off-by: Casper Guldbech Nielsen <scni@novonordisk.com>

--- cookbook/agents/weather/tools.py
+++ cookbook/agents/weather/tools.py
@@ -163,8 +163,7 @@ class HistoricalWeather(AgentTool):
             )
 
         if (
-            datetime.strptime(end_date, "%Y-%m-%d")
-            - datetime.strptime(start_date, "%Y-%m-%d")
+            datetime.strptime(end_date, "%Y-%m-%d") - datetime.strptime(start_date, "%Y-%m-%d")
         ).days > 30:
             raise ValueError(
                 "The time span between start_date and end_date cannot exceed 30 days."
