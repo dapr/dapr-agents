7a38aaf810f14458b431e370f218c8a013efa8d0
Casper Guldbech Nielsen <scni@novonordisk.com>
2025-04-13 16:43:24 +0200

    Fix: F811
    Signed-off-by: Casper Guldbech Nielsen <scni@novonordisk.com>

--- dapr_agents/types/llm.py
+++ dapr_agents/types/llm.py
@@ -3,10 +3,6 @@ from pydantic import BaseModel, Field, model_validator, field_validator, ConfigD
 from pydantic_core import PydanticUseDefault
 from pathlib import Path
 from io import BytesIO, BufferedReader
-
-from pydantic import BaseModel, Field
-
-
 class ElevenLabsClientConfig(BaseModel):
     base_url: Literal[
         "https://api.elevenlabs.io", "https://api.us.elevenlabs.io"
