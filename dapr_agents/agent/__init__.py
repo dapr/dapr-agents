1e0fa6b6444a021a7abf8745b7b50718d626af38
Casper Guldbech Nielsen <scni@novonordisk.com>
2025-05-03 12:12:17 +0200

    Chore: export DaprAgentsOTel out to dapr_agents.agent
    Signed-off-by: Casper Guldbech Nielsen <scni@novonordisk.com>

--- dapr_agents/agent/__init__.py
+++ dapr_agents/agent/__init__.py
@@ -2,3 +2,4 @@ from .base import AgentBase
 from .utils.factory import Agent
 from .actor import AgentActor
 from .patterns import ReActAgent, ToolCallAgent, OpenAPIReActAgent
+from .telemetry import DaprAgentsOTel
