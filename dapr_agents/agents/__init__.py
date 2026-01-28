1c76468354e0151c07c1fdeabbff38b69e0ca692
Casper Nielsen <casper@diagrid.io>
2026-01-28 18:42:37 +0100

    chore: remove exports of removed funcs
    Signed-off-by: Casper Nielsen <casper@diagrid.io>

--- dapr_agents/agents/__init__.py
+++ dapr_agents/agents/__init__.py
@@ -1,25 +1,9 @@
 from .base import AgentBase
 from .standalone import Agent
 from .durable import DurableAgent
-from .schemas import (
-    AgentMetadataSchema,
-    AgentMetadata,
-    LLMMetadata,
-    ToolMetadata,
-    RegistryMetadata,
-    MemoryMetadata,
-    PubSubMetadata,
-)
 
 __all__ = [
     "AgentBase",
     "Agent",
     "DurableAgent",
-    "AgentMetadataSchema",
-    "AgentMetadata",
-    "LLMMetadata",
-    "ToolMetadata",
-    "RegistryMetadata",
-    "MemoryMetadata",
-    "PubSubMetadata",
 ]
