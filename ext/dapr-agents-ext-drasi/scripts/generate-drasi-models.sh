2992e544b0e62e9f11a1e018dfc35c380081ad0a
Jeffrey Zhang <jeffreyjpizhang@gmail.com>
2026-06-26 17:22:41 -0700

    chore: add license header to drasi models script
    Signed-off-by: Jeffrey Zhang <jeffreyjpizhang@gmail.com>

--- ext/dapr-agents-ext-drasi/scripts/generate-drasi-models.sh
+++ ext/dapr-agents-ext-drasi/scripts/generate-drasi-models.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
+#
+# Copyright 2026 The Dapr Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 set -euo pipefail
 
 EXT_DIR="."
 SCHEMA_DIR="$EXT_DIR/schemas/output-unpacked"
-OUT_DIR="$EXT_DIR/dapr_agents/ext/drasi/schemas/unpacked"
+OUT_DIR="$EXT_DIR/dapr_agents/ext/drasi/schemas/generated/unpacked"
 
 # Remove output directory to avoid stale generated models
 rm -rf "$OUT_DIR"
