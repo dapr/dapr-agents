60e50b9d4ad67b0fa286c521182dd6fc448a738a
Jeffrey Zhang <jeffreyjpizhang@gmail.com>
2026-07-08 21:22:13 -0700

    chore: switch import order
    Signed-off-by: Jeffrey Zhang <jeffreyjpizhang@gmail.com>

--- ext/dapr-agents-ext-drasi/dapr_agents/ext/drasi/__init__.py
+++ ext/dapr-agents-ext-drasi/dapr_agents/ext/drasi/__init__.py
@@ -11,7 +11,7 @@
 # limitations under the License.
 #
 
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
 
 from .activations import drasi_trigger
 from .types import DrasiChangeEvent, DrasiOperation
